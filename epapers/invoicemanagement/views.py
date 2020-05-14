d29614415af0f7002f641ba2514752aef91fb6ad
GuillermoMoran <guillermoalexandermoran@gmail.com>
2020-05-14 02:58:23 -0400

    Añadí las funciones del view.. (sin codigo)

--- epapers/invoicemanagement/views.py
+++ epapers/invoicemanagement/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from PyPDF2 import PdfFileWriter, PdfFileReader
+import io
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter, A4
 
-# Create your views here.
+
+def SignIn(Request):
+	pass
+
+
+def Index(Request):
+	pass
+
+
+def SupplierInvoiceList(Request):
+	pass
+
+
+def UploadInvoice(Request):
+	pass
+
+
+def CreateOrder():
+	pass
+
+
+def SendInvoice():
+	pass
+
+
+def SignInvoice(Request):
+	pass
+
+
+def StoreInvoiceList(Request):
+	pass
